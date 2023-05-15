5dc364e578426ed915d441e5eed6960f4faa12cd
Ikechukwu Agbarakwe <iaweepingsaint@gmail.com>
2023-05-15 05:51:48 +0100

    Sort and print apparels and their prices

--- pages/home_page.py
+++ pages/home_page.py
@@ -9,21 +9,23 @@ class HomePage(BasePage):
     def __init__(self, driver):
         super().__init__(driver)
         self.sign_in_link = self.driver.find_element(By.CLASS_NAME, "login")
-        self.bestsellers_link = self.driver.find_element(
-            By.LINK_TEXT, "Best Sellers")
-        self.product_labels = self.driver.find_elements(
-            By.CSS_SELECTOR, ".right-block product-name")
-        self.product_prices = self.driver.find_elements(
-            By.CSS_SELECTOR, ".right-block .price.product-price")
 
     def goto_login(self):
         self.sign_in_link.click()
         self.wait.until(EC.title_is("Login - My Store"))
 
     def get_bestsellers(self):
+        self.bestsellers_link = self.driver.find_element(
+            By.CSS_SELECTOR, "a.blockbestsellers")
         self.bestsellers_link.click()
+
+        self.product_labels = self.driver.find_elements(
+            By.CSS_SELECTOR, ".right-block a.product-name")
+        self.product_prices = self.driver.find_elements(
+            By.CSS_SELECTOR, ".right-block span.price.product-price")
 
         price_list = list(zip(self.product_labels, self.product_prices))
         sorted_price_list = sorted(price_list, key=lambda x: x[1].text[1:])
-        print(price_list)
-        print(sorted_price_list)
+
+        for item in sorted_price_list:
+            print(item[0].text, item[1].text)
--- web_test.py
+++ web_test.py
@@ -3,6 +3,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
+from pages import home_page
 
 from pages.home_page import HomePage
 from pages.login_page import LoginPage
@@ -13,6 +14,8 @@ class AutomationSite():
     def __init__(self, driver):
         self.driver = driver
         self.url = "http://automationpractice.pl/index.php"
+        # self.home_page = HomePage(driver)
+        # self.login_page = LoginPage(driver)
 
     def open_site(self):
         self.driver.get(self.url)
@@ -20,10 +23,13 @@ class AutomationSite():
         assert "My Store" in self.driver.title
 
     def login(self):
-        home_page = HomePage(driver)
-        home_page.goto_login()
-        login_page = LoginPage(driver)
-        login_page.signin()
+        self.home_page = HomePage(driver)
+        self.home_page.goto_login()
+        self.login_page = LoginPage(driver)
+        self.login_page.signin()
+
+    def print_products(self):
+        self.home_page.get_bestsellers()
 
 
 if __name__ == "__main__":
@@ -31,5 +37,6 @@ if __name__ == "__main__":
     auto_site = AutomationSite(driver)
     auto_site.open_site()
     auto_site.login()
+    auto_site.print_products()
 
     sleep(5)
